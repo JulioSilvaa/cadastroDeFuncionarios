7f4b70bf7b7800d2bf6ab9b2686b1e9bf7e80c92
julio cesar da silva <juliocesar.amancio@yahoo.com.br>
2023-01-27 16:44:01 -0300

    finalizando a tabela com dados dos funcionários cadastrados

--- src/pages/EmployeesList.js
+++ src/pages/EmployeesList.js
@@ -1,20 +1,46 @@
+import { DataGrid } from "@mui/x-data-grid";
 import { useFetchEmployeeshDoc } from "hooks/useFetchEmployeesDoc";
 
 export default function EmployeesList() {
   const { documents: employees } = useFetchEmployeeshDoc("funcionarios");
 
-  const employee = employees?.map((item) => (
-    <li>
-      {item.firstname}
-      <img src={item.image} alt="" />
-    </li>
-  ));
+  const employee = employees && employees;
+  const rows = employee;
 
-  console.log(employees);
+  const columns = [
+    { field: "firstname", headerName: "Nome", width: 130 },
+    { field: "lastname", headerName: "Sobrenome", width: 130 },
+    {
+      field: "age",
+      headerName: "idade",
+      type: "number",
+      width: 70,
+    },
+    { field: "job", headerName: "Cargo", width: 200 },
+    { field: "email", headerName: "Email", width: 200 },
+    { field: "nationality", headerName: "Nacionalidade", width: 200 },
+    { field: "sector", headerName: "Setor", width: 200 },
+    { field: "wage", headerName: "Salário", width: 130 },
+    { field: "birthdate", headerName: "Data de Nascimento", width: 130 },
+    { field: "startOfContract", headerName: "Início de contrato", width: 130 },
+    { field: "telephone", headerName: "Telefone", width: 130 },
+    { field: "description", headerName: "Descrição", width: 300 },
+  ];
 
-  return (
-    <div>
-      <ul>{employee}</ul>
-    </div>
-  );
+  function getid(e) {
+    console.log(e.row);
+  }
+
+  if (rows)
+    return (
+      <div style={{ height: 400, width: "90%", margin: "20px auto" }}>
+        <DataGrid
+          onRowClick={getid}
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[6]}
+        />
+      </div>
+    );
 }
